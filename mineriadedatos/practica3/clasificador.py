ea469f8babcbf7c53caa06ed1c2f2a4699833cf6
Unknown <sodomangelriper@gmail.com>
2019-11-12 17:50:35 +0100

    añadido rango de freidman

--- mineriadedatos/practica3/clasificador.py
+++ mineriadedatos/practica3/clasificador.py
@@ -9,56 +9,65 @@ from sklearn.metrics import mean_squared_error
 from scipy.stats import wilcoxon
 from os import listdir
 import numpy as np
+from scipy.stats import friedmanchisquare
+from scipy.stats import
+
+#ejecutar con python2.7
 
 datasets=listdir('./datasets')
 
 scoreWilKNN=[]
 scoreWilSVM=[]
-for j in range(2):
+scoreWilTREE=[]
 
-    for i in datasets:
+for i in datasets:
 
-        print('Base de datos: '+ str(i))
-        dataset = arff.loadarff('./datasets/'+str(i))
-        df = pd.DataFrame(dataset[0])
-        data=df.iloc[:, df.columns != 'class']
-        target=pd.factorize(df['class'])[0]
-        X_train,X_test,Y_train,Y_test=train_test_split(data, target, test_size=0.4)
+    print('Base de datos: ' + str(i))
+    dataset = arff.loadarff('./datasets/' + str(i))
+    df = pd.DataFrame(dataset[0])
+    data = df.iloc[:, df.columns != 'class']
+    target = pd.factorize(df['class'])[0]
+    X_train, X_test, Y_train, Y_test = train_test_split(data, target, test_size=0.4)
 
-        #Llamada y entrenamiento del algoritmo KNN
-        knn=KNeighborsClassifier(n_neighbors=5)
-        knn.fit(X_train,Y_train)
-        print('Porcentaje de bien clasificados KNN:')
-        print(knn.score(X_test,Y_test))
-        scoreWilKNN.append(knn.score(X_test,Y_test))
-        array=knn.predict([df.iloc[5,  df.columns != 'class']])
-        print('Clase predicha KNN')
-        print(array)
-        print('----------------')
+    # Llamada y entrenamiento del algoritmo KNN
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, Y_train)
+    print('Porcentaje de bien clasificados KNN:')
+    print(knn.score(X_test, Y_test))
+    scoreWilKNN.append(knn.score(X_test, Y_test))
+    array = knn.predict([df.iloc[5, df.columns != 'class']])
+    print('Clase predicha KNN')
+    print(array)
+    print('----------------')
 
-        #llamada y entrenamiento algoritmo SVM
-        svm=SVC(gamma='auto')
-        svm.fit(X_train,Y_train)
-        print('Porcentaje de bien clasificados SVM')
-        print(svm.score(X_test,Y_test))
-        scoreWilSVM.append(knn.score(X_test, Y_test))
-        array=knn.predict([df.iloc[5,  df.columns != 'class']])
-        print('Clase predicha SVM')
-        print(array)
+    # llamada y entrenamiento algoritmo SVM
+    svm = SVC(gamma='auto')
+    svm.fit(X_train, Y_train)
+    print('Porcentaje de bien clasificados SVM')
+    print(svm.score(X_test, Y_test))
+    scoreWilSVM.append(knn.score(X_test, Y_test))
+    array = knn.predict([df.iloc[5, df.columns != 'class']])
+    print('Clase predicha SVM')
+    print(array)
 
-        print('-----------------')
+    print('-----------------')
 
-        #llamada y entrenamiento del arbol de decision
-        arbol=DecisionTreeClassifier()
-        arbol=arbol.fit(X_train,Y_train)
-        print('Porcentaje de bien clasificados arbol')
-        print(arbol.score(X_test,Y_test))
-        array=arbol.predict([df.iloc[5,  df.columns != 'class']])
-        print('Clase predicha arbol')
-        print(array)
+    # llamada y entrenamiento del arbol de decision
+    arbol = DecisionTreeClassifier()
+    arbol = arbol.fit(X_train, Y_train)
+    print('Porcentaje de bien clasificados arbol')
+    print(arbol.score(X_test, Y_test))
+    scoreWilTREE.append(arbol.score(X_test, Y_test))
+    array = arbol.predict([df.iloc[5, df.columns != 'class']])
+    print('Clase predicha arbol')
+    print(array)
 
-        print('-----------------------------------------------')
+    print('-----------------------------------------------')
 
 
 print('Test de Wilcoxon para SVM y KNN de todos los datasets')
 print(wilcoxon(x=scoreWilKNN, y=scoreWilSVM, zero_method='zsplit', correction=False))
+
+print('Rango de Friedman')
+print(friedmanchisquare(scoreWilKNN,scoreWilSVM,scoreWilTREE))#Minimo 3 argumentos
+
